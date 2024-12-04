cc8200cb4c1eaeb81d25fb18db76c66cda263be1
connetium-fatih <fatih16bilgin@gmail.com>
2024-12-04 18:01:42 +0300

    Odev1 Completed

---
+++ Odev1/FatihBilginOdev1.kt
@@ -0,0 +1,100 @@
+package com.fatihbilgin.kotlindersleri.Odev1
+
+fun main() {
+    val ilce = "Üsküdar"
+    println("Ilce: $ilce")
+    val kita = "Asya"
+    println("Kita: $kita")
+    val faks = "02165551234"
+    println("Faks: $faks")
+    val postaKodu = 16120
+    println("Posta Kodu: $postaKodu")
+    val instaAdresi = "@thebillgin"
+    println("Insta Adresi: $instaAdresi")
+    val calistigiBolum = "IT"
+    println("Calistigi Bolum: $calistigiBolum")
+    val urunMiktari = 10
+    println("Urun Miktari: $urunMiktari")
+    val musteriSoyadi = "Bilgin"
+    println("Musteri Soyadi: $musteriSoyadi")
+    val odemeMiktari = 110.15
+    println("Odeme Miktari: $odemeMiktari")
+    val dogumTarihi = "19-01-2000"
+    println("Dogum Tarihi: $dogumTarihi")
+    val borc = 100.0
+    println("Borc: $borc")
+    val medeniHali = "Bekar"
+    println("Medeni Hali: $medeniHali")
+    val videoYorum = "Harika bir içerik"
+    println("Video Yorum: $videoYorum")
+    val odemeSaati = "11:30"
+    println("Ödeme Saati: $odemeSaati")
+    val eftMiktari = 2110.0
+    println("EFT Miktarı: $eftMiktari")
+    val satilanMiktar = 10
+    println("Satılan Miktar: $satilanMiktar")
+    val telefonModeli = "iPhone 11"
+    println("Telefon Modeli: $telefonModeli")
+    val dergiAdi = "PENGUEN Dergisi"
+    println("Dergi Adı: $dergiAdi")
+    val basimTarihi = "04-12-2024"
+    println("Basım Tarihi: $basimTarihi")
+    val zamMiktari = 110.3
+    println("Zam Miktarı: $zamMiktari")
+    val daireSayisi = 5
+    println("Daire Sayısı: $daireSayisi")
+    val enlem = 41.0082
+    println("Enlem: $enlem")
+    val boylam = 28.9784
+    println("Boylam: $boylam")
+    val yemekAdi = "Kebap"
+    println("Yemek Adı: $yemekAdi")
+    val urunFiyati = 15.99
+    println("Ürün Fiyatı: $urunFiyati")
+    val sirket = "FMSS"
+    println("Şirket: $sirket")
+    val videoAdi = "Kotlin Eğitim Videosu"
+    println("Video Adı: $videoAdi")
+    val muzikSuresi = "2:45"
+    println("Müzik Süresi: $muzikSuresi")
+    val mekanPuani = 4.3
+    println("Mekan Puanı: $mekanPuani")
+    val dosyaAdi = "odev1.kt"
+    println("Dosya Adı: $dosyaAdi")
+    val resimFormati = "png"
+    println("Resim Formatı: $resimFormati")
+    val renk = "Mavi"
+    println("Renk: $renk")
+    val renkKodu = "#0000FF"
+    println("Renk Kodu: $renkKodu")
+    val bilgisayarModeli = "MacBook M1 Air"
+    println("Bilgisayar Modeli: $bilgisayarModeli")
+    val ekranBoyutu = 13.6
+    println("Ekran Boyutu: $ekranBoyutu")
+    val kullanimSuresi = "2 saat"
+    println("Kullanım Süresi: $kullanimSuresi")
+    val basinc = "1013 hPa"
+    println("Basınç: $basinc")
+    val etkinlikGunu = "Cumartesi"
+    println("Etkinlik Günü: $etkinlikGunu")
+    val odemeGunu = "Pazartesi"
+    println("Ödeme Günü: $odemeGunu")
+    val yolculukCikisTarihi = "03-12-2024"
+    println("Yolculuk Çıkış Tarihi: $yolculukCikisTarihi")
+    val mahalleAdi = "Ahmediye"
+    println("Mahalle Adı: $mahalleAdi")
+    val otobusHatti = "1T"
+    println("Otobüs Hattı: $otobusHatti")
+    val kullanilanDakika = 410
+    println("Kullanılan Dakika: $kullanilanDakika")
+    val kargoTakipNo = "ABC1234567890"
+    println("Kargo Takip No: $kargoTakipNo")
+    val kuponSuresi = "10 gün"
+    println("Kupon Süresi: $kuponSuresi")
+    val kuponKodu = "INDIRIM50"
+    println("Kupon Kodu: $kuponKodu")
+    val faturaTarihi = "04-12-2024"
+    println("Fatura Tarihi: $faturaTarihi")
+
+
+}
